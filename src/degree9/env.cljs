999dacd3e078f91e38317366487a1079c6b3207e
Matthew Ratzke <matt@degree9.io>
2018-07-17 00:13:22 -0600

    refactor degree9.env

--- src/degree9/env.cljs
+++ src/degree9/env.cljs
@@ -1,41 +1,33 @@
 (ns degree9.env
- (:require
-  cljs.nodejs
-  camel-snake-kebab.core
-  goog.object
-  [cljs.test :refer-macros [deftest is]]
-  ["dotenv" :as dotenv])
- (:refer-clojure :exclude [get]))
+  (:refer-clojure :exclude [get keys])
+  (:require cljs.nodejs
+            [cuerdas.core :as str]
+            [goog.object :as obj]
+            [cljs.test :refer-macros [deftest is]]
+            ["dotenv" :as dotenv]))
 
-(let [config (atom nil)]
- (defn env-obj
-  "inits dotenv and returns the JS object where env values can be found"
+(def ^:dynamic *env* (atom nil))
+
+(defn env-obj
+  "Initialize dotenv and return the process.env object."
+  [& conf]
+  (when-not @*env*
+   (reset! *env* (.config dotenv conf)))
+  (obj/get js/process "env"))
+
+(defn keys
+  "Return all keys from the process.env object."
   []
-  (when-not @config
-   (reset! config (.config dotenv)))
-  (.-env js/process)))
-
-(defn dir
- "returns the set of all available env keywords"
- []
- (set
-  (map
-   camel-snake-kebab.core/->kebab-case-keyword
-   (js->clj
-    (js-keys
-     (env-obj))))))
+  (->> (env-obj)
+    (js-keys)
+    (js->clj)
+    (map str/kebab)
+    (map keyword)))
 
 (defn get
- "returns the env value string for key, with optional fallback or empty string"
- ([k] (get k nil))
- ([k fallback]
-  {:post [(string? %)]}
-  (str
-   (if-let [v (goog.object/get
-               (env-obj)
-               (camel-snake-kebab.core/->SCREAMING_SNAKE_CASE_STRING
-                (name k)))]
-    v fallback))))
+  "Return the process.env object value for `key` or `default`."
+  ([key] (get key nil))
+  ([key default] (obj/get (env-obj) (-> key name str/snake str/upper) default)))
 
 ; TESTS
 
@@ -43,13 +35,13 @@
  (doseq [e [:pwd :home :user]]
   (is
    (contains?
-    (dir)
+    (keys)
     e))))
 
 (deftest ??get
  (is (clojure.string/includes? (get :pwd) "enterprise"))
  (is (clojure.string/includes? (get "PWD") "enterprise"))
- ; no fallback yields "" for no match
- (is (= "" (get :no-match)))
+ ; no fallback yields nil for no match
+ (is (= nil (get :no-match)))
  ; should fallback to foo with no match
  (is (= "foo" (get :no-match "foo"))))
