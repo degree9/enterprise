b025367d497f219bc6a2ff4b96e46b04e870e2e5
Matthew Ratzke <matt@degree9.io>
2018-10-08 15:55:18 -0600

    update degree9.remote

---
+++ src/degree9/remote.cljs
@@ -0,0 +1,17 @@
+(ns degree9.remote
+  "Provides methods for connecting via microservices pattern."
+  (:require
+    [meta.server :as server]
+    [meta.client :as client]))
+
+(defn connection
+  "Create a connection to a remote server."
+  [uri]
+  (-> (client/app)
+    (client/with-socketio uri)
+    (client/with-authentication)))
+
+(defn api
+  "Mount a remote service to a local endpoint."
+  [app path conn service hooks]
+  (server/api app path (client/service conn service) hooks))
