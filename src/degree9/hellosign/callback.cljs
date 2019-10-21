08374457a7831a35fd538634fd9179ff3aafb1a9
David Meister <thedavidmeister@gmail.com>
2019-10-21 22:57:26 +1100

    data from hellosign callbacks

--- src/degree9/hellosign/callback.cljs
+++ src/degree9/hellosign/callback.cljs
@@ -1,14 +1,23 @@
-(ns degree9.hellosign.callback)
+(ns degree9.hellosign.callback
+ (:require
+  degree9.multipart-form))
 
 (defn all-signed! [])
 
 (defn hellosign-service [& opts]
   (reify Object
-    (create [this & args]
+    (create [this data params]
       (let []
-        (.log js/console args)
-        (js/Promise. (fn [resolve reject] (resolve "Hello API Event Received")))))))
+       (prn
+        (js->clj
+         (.parse js/JSON
+          (.-json data))
+         :keywordize-keys true))
+       (prn (js->clj params))
+       (js/Promise. (fn [resolve reject] (resolve "Hello API Event Received")))))))
 
 (defn with-callback! [app]
-  (.use app "/hellosign/callback"
-    (hellosign-service)))
+ (degree9.multipart-form/multipart-none
+  app
+  "/hellosign/callback"
+  (hellosign-service)))
