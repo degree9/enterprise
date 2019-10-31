19e8cf86ed93c16f43e205cd3bae558da954dd17
Matthew Ratzke <matt@degree9.io>
2019-10-31 16:14:51 -0600

    add server roles

---
+++ src/degree9/roles.cljs
@@ -0,0 +1,32 @@
+(ns degree9.roles
+  (:require ["debug" :as dbg]
+            [goog.object :as obj]
+            [degree9.hooks :as hooks]
+            [covenant.core :as cov]
+            [covenant.rbac]))
+
+(def ^:private debug (dbg "degree9:enterprise:roles"))
+
+(defn- not-authorized []
+  (throw (js/Error. "User does not have sufficient authorization.")))
+
+(defn require-roles [roles]
+  (fn [hook]
+    (debug "Validate required user roles: " roles)
+    (let [user (get-in (hooks/params hook) ["user"])]
+      (if-not (cov/validate {"roles" (js->clj roles)} (js->clj user)) (not-authorized)
+        hook))))
+
+(defn authentication-roles [roles]
+  (fn [hook]
+    (debug "Authenticate user with roles: " roles)
+    (let [user (get-in (hooks/result hook) ["user"])]
+      (if-not (cov/validate {"roles" (js->clj roles)} (js->clj user)) (not-authorized)
+        hook))))
+
+(defn with-authorization [app]
+  (debug "Loading Role Authorization api")
+  (let [roles (obj/get (.get app "authorization") "roles")]
+    (.hooks (.service app "authentication")
+      (clj->js {:after {:all [(authentication-roles roles)]}}))
+    app))
