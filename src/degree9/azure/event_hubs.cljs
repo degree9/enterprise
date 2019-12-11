63fdbe9c0273451dbfb38dd7c620e2f453caea7a
Matthew Ratzke <matt@degree9.io>
2019-12-10 18:27:13 -0700

    wip

--- src/degree9/azure/event_hubs.cljs
+++ src/degree9/azure/event_hubs.cljs
@@ -3,28 +3,32 @@
             [degree9.env :as env]
             ["@azure/event-hubs" :as event]))
 
-;; Azure Service Bus ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def producer-client (obj/get event "EventHubsProducerClient"))
+;; Azure Event Hubs ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def event-hubs (obj/get event "EventHubClient"))
 
-(def consumer-client (obj/get event "EventHubConsumerClient"))
+(defn connection-string [conn eventhub]
+  (.createFromConnectionString event-hubs conn eventhub))
+
+(defn send! [client message]
+  (.send client message))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Azure Service Bus Queue Service ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn Producer [eventhub & [opts]]
+;; Azure Event Hubs Service ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn EventHub [eventhub & [opts]]
   (let [endpoint (:endpoint opts (env/get "AZURE_EVENTHUBS_CONNECTION_STRING"))
-        producer (producer-client. endpoint eventhub)]
+        ehubs    (connection-string endpoint eventhub)]
     (reify
       Object
+      (find [this params]
+        (send! ehubs (clj->js {:body {:type "find"   :id nil :data nil}  :contentType "application/json"})))
+      (get [this id params]
+        (send! ehubs (clj->js {:body {:type "get"    :id id  :data nil}  :contentType "application/json"})))
       (create [this data params]
-        (let [batch (.createBatch producer)]
-          (.tryAdd batch (clj->js {:body data}))
-          (.sendBatch producer batch))))))
-
-(defn Consumer [& [opts]]
-  (let [endpoint (:endpoint opts (env/get "AZURE_EVENTHUBS_CONNECTION_STRING"))
-        group (:group opts)
-        consumer (consumer-client. group endpoint eventhub)]
-    (reify
-      Object
-      (create [this data params]))))
+        (send! ehubs (clj->js {:body {:type "create" :id nil :data data} :contentType "application/json"})))
+      (update [this id data params]
+        (send! ehubs (clj->js {:body {:type "update" :id id  :data data} :contentType "application/json"})))
+      (patch [this id data params]
+        (send! ehubs (clj->js {:body {:type "patch"  :id id  :data data} :contentType "application/json"})))
+      (remove [this id params]
+        (send! ehubs (clj->js {:body {:type "remove" :id id  :data nil}  :contentType "application/json"}))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- src/degree9/azure/service_bus.cljs
+++ src/degree9/azure/service_bus.cljs
@@ -4,10 +4,16 @@
             [degree9.env :as env]
             ["@azure/service-bus" :as sbus]))
 
+;; Service Bus Receive Mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def receive-mode (obj/get sbus "ReceiveMode"))
+
+(def peek-lock (.-peekLock receive-mode))
+
+(def receive-delete (.-receiveAndDelete receive-mode))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Azure Service Bus ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def service-bus (obj/get sbus "ServiceBusClient"))
-
-(def receive-mode (obj/get sbus "ReceiveMode"))
 
 (defn connection-string [conn & [opts]]
   (.createFromConnectionString service-bus conn (clj->js opts)))
@@ -30,28 +36,79 @@
 (defn receiver [client mode]
   (.createReceiver client mode))
 
-(def peek-lock (.-peekLock reveive-mode))
+(defn register-message-handler [client msg err & [opts]]
+  (.registerMessageHandler client msg err opts))
 
+(defn- message-handler [service]
+  (fn [msg]
+    (let [{:keys [body]} (js->clj msg :keywordize-keys true)]
+      (.emit service (:type body) (dissoc body :type)))))
+
+(defn- error-handler [this]
+  (fn [err]
+    (throw (js/Error. err))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Azure Service Bus Queue Service ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn QueuePublisher [queue & [opts]]
+(defn queue [queue & [opts]]
   (let [endpoint (:endpoint opts (env/get "AZURE_SERVICEBUS_CONNECTION_STRING"))
+        mode (:receive-mode opts peek-lock)
         sbus (queue-client (connection-string endpoint) queue)
-        sender (sender sbus)]
+        opts (select-keys opts [:autoComplete :maxConcurrentCalls :maxMessageAutoRenewLockDurationInSeconds])
+        sender (sender sbus)
+        receiver (receiver sbus mode)]
     (reify
       Object
+      (setup [this app path]
+        (obj/set this "sender" sender)
+        (obj/set this "receiver" receiver)
+        (register-message-handler receiver (message-handler this) (error-handler this) (clj->js opts)))
+      (find [this params]
+        (send! sender (clj->js {:body {:type "find"   :id nil :data nil}  :contentType "application/json"})))
+      (get [this id params]
+        (send! sender (clj->js {:body {:type "get"    :id id  :data nil}  :contentType "application/json"})))
       (create [this data params]
-        (send! sender (clj->js {:body data :contentType "application/json"}))))))
+        (send! sender (clj->js {:body {:type "create" :id nil :data data} :contentType "application/json"})))
+      (update [this id data params]
+        (send! sender (clj->js {:body {:type "update" :id id  :data data} :contentType "application/json"})))
+      (patch [this id data params]
+        (send! sender (clj->js {:body {:type "patch"  :id id  :data data} :contentType "application/json"})))
+      (remove [this id params]
+        (send! sender (clj->js {:body {:type "remove" :id id  :data nil}  :contentType "application/json"}))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Azure Service Bus Topic Service ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn TopicPublisher [topic & [opts]]
+(defn topic [topic & [opts]]
   (let [endpoint (:endpoint opts (env/get "AZURE_SERVICEBUS_CONNECTION_STRING"))
         sbus (topic-client (connection-string endpoint) topic)
         sender (sender sbus)]
     (reify
       Object
+      (setup [this app path]
+        (obj/set this "sender" sender))
+      (find [this params]
+        (send! sender (clj->js {:body {:type "find"   :id nil :data nil}  :contentType "application/json"})))
+      (get [this id params]
+        (send! sender (clj->js {:body {:type "get"    :id id  :data nil}  :contentType "application/json"})))
       (create [this data params]
-        (send! sender (clj->js {:body data :contentType "application/json"}))))))
+        (send! sender (clj->js {:body {:type "create" :id nil :data data} :contentType "application/json"})))
+      (update [this id data params]
+        (send! sender (clj->js {:body {:type "update" :id id  :data data} :contentType "application/json"})))
+      (patch [this id data params]
+        (send! sender (clj->js {:body {:type "patch"  :id id  :data data} :contentType "application/json"})))
+      (remove [this id params]
+        (send! sender (clj->js {:body {:type "remove" :id id  :data nil}  :contentType "application/json"}))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Azure Service Bus Topic Service ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn subscription [topic & [opts]]
+  (let [endpoint (:endpoint opts (env/get "AZURE_SERVICEBUS_CONNECTION_STRING"))
+        mode (:receive-mode opts peek-lock)
+        sbus (subscription-client (connection-string endpoint) topic)
+        receiver (receiver sbus mode)]
+    (reify
+      Object
+      (setup [this app path]
+        (obj/set this "receiver" receiver)
+        (register-message-handler receiver (message-handler this) (error-handler this) (clj->js opts))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
