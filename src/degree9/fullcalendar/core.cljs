9be4b3bf85c5172395e445699c9f1aa0cf9432f9
Nolan Jelley <nolanbjelley@gmail.com>
2020-04-22 19:03:34 -0600

    added some commands

---
+++ src/degree9/fullcalendar/core.cljs
@@ -0,0 +1,46 @@
+(ns degree9.fullcalendar.core
+  (:require [hoplon.core :as h]
+            [goog.object :as obj]
+            ["@fullcalendar/core" :as fullcal]
+            [degree9.fullcalendar.plugins :as plugins]))
+
+(defn mkcal [el & [opts]]
+  (let [calendar (obj/get fullcal "Calendar")]
+    (calendar. el (clj->js opts))))
+
+(h/defelem calendar [attr kids]
+  (let [cal (h/div)
+        plugins (:plugins attr #js["dayGrid"]);plugins/daygrid plugins/timegrid plugins/list])
+        fcal (mkcal cal {:plugins plugins})]
+    (.render fcal)
+    cal))
+
+(defn render
+  "Will initially render a calendar, or if it is already rendered, will rerender it"
+  [cal]
+  (.render cal))
+
+(defn destroy
+  "Restores the container element to the state before FullCalendar"
+  [cal]
+  (.destroy cal))
+
+(defn batch-rendering
+  "A way to group operations that cause rerenders"
+  [cal]
+  (.batchRendering cal))
+
+(defn get-option
+  "A way to group operations that cause rerenders"
+  [cal & [opts]]
+  (.getOption cal opts))
+
+(defn set-option
+  "A way to group operations that cause rerenders"
+  [cal & [opts]]
+  (.setOption cal opts))
+
+(defn goto-date
+  "Moves the calendar to an arbitraty date"
+  [cal date]
+  (.gotoDate cal date))
