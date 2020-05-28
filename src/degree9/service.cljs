ebb62cfe3739b7180c3a7893e1750b77c6b5c614
Matthew Ratzke <matt@degree9.io>
2020-05-28 13:48:43 -0600

    initial degree9.service

--- src/degree9/service.cljs
+++ src/degree9/service.cljs
@@ -1,49 +1,50 @@
 (ns degree9.service
- (:require ["socket.io-client" :as io]
-           ["@feathersjs/feathers" :as feathers]
-           ["@feathersjs/socketio-client" :as socketio]
-           [goog.object :as obj]
-           [meta.server :as server]
-           [degree9.debug :as dbg]))
+  "A microservice proxy via websockets."
+  (:require ["socket.io-client" :as io]
+            ["@feathersjs/feathers" :as feathers]
+            ["@feathersjs/socketio-client" :as socketio]
+            [goog.object :as obj]
+            [meta.server :as server]
+            [degree9.debug :as dbg]))
 
 (dbg/defdebug debug "degree9:enterprise:service")
 
-(defn mksocket
-  "Create a websocket connection to remote microservice."
-  [client uri]
-  (debug "Create websocket connection to remote microservice at %s" uri)
-  (let [socket (io uri)]
-    (doto client
-      (.configure (socketio socket)))))
+(defn- wsclient
+  "Configure feathers socket.io client"
+  [client socket]
+  (debug "Configure feathers socket.io client")
+  (doto client
+    (.configure (socketio socket))))
 
 (defn µservice [uri service & [opts]]
-  (debug "Initializing Remote Microservice at %s/%s" uri service)
-  (let [client (:client opts (feathers))
-        id (:id opts "id")]
-    (reify
-      Object
-      (id [this] id)
-      (setup [this app path]
-        (obj/set this "proxy"
-          (.service (mksocket client uri) service)))
-      (find [this params]
-        (prn "µService FIND" params)
-        (-> (.find (obj/get this "proxy") params)
-          (.then prn)
-          (.catch prn)))
-      (get [this id params]
-        (.get (obj/get this "proxy") id params))
-      (create [this data params]
-        (.create (obj/get this "proxy") data params))
-      (update [this id data params]
-        (.update (obj/get this "proxy") id data params))
-      (patch [this id data params]
-        (.patch (obj/get this "proxy") id data params))
-      (remove [this id params]
-        (.remove (obj/get this "proxy") id params)))))
+  (debug "Initializing remote microservice %s" uri)
+  (reify
+    Object
+    (id [this] (:id opts "id"))
+    (setup [this app path]
+      (let [client (:client opts (feathers))
+            conf   (clj->js (dissoc opts :id :client))
+            socket (io uri conf)
+            proxy  (wsclient client socket)]
+        (debug "Setup proxy to remote service %s" service)
+        (obj/set this "io" (.service proxy service))))
+    (find [this params]
+      (.find (obj/get this "io") params))
+    (get [this id params]
+      (.get (obj/get this "io") id params))
+    (create [this data params]
+      (.create (obj/get this "io") data params))
+    (update [this id data params]
+      (.update (obj/get this "io") id data params))
+    (patch [this id data params]
+      (.patch (obj/get this "io") id data params))
+    (remove [this id params]
+      (.remove (obj/get this "io") id params))))
 
 (defn api
   "Mount a remote Microservice to a local endpoint."
-  [app path uri service hooks]
-  (debug "Mount Remote Microservice at %s" path)
-  (server/api app path (µservice uri service) hooks))
+  ([app path uri service hooks]
+   (api app path uri {} service hooks))
+  ([app path uri opts service hooks]
+   (debug "Mount remote microservice at %s" path)
+   (server/api app path (µservice uri service opts) hooks)))
