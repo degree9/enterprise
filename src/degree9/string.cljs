4f99bc822ee1314ad3acf74e66215162789d077c
Matthew Ratzke <matt@degree9.io>
2020-07-21 17:55:26 -0600

    wip

--- src/degree9/string.cljs
+++ src/degree9/string.cljs
@@ -4,3 +4,9 @@
             [goog.string.format]))
 
 (def format gstr/format)
+
+(def join cstr/join)
+
+(defn pad-start
+  ([s length] (.padStart (str s) length))
+  ([s length pad] (.padStart (str s) length pad)))
