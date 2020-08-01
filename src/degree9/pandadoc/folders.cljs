c8a2fb78c95d551095cd6f9ce7f4c15838343d23
Nolan Jelley <nolanbjelley@gmail.com>
2020-08-01 15:04:11 -0600

    Update folders.cljs

--- src/degree9/pandadoc/folders.cljs
+++ src/degree9/pandadoc/folders.cljs
@@ -3,19 +3,19 @@
 
 ;; Folders for templates and documents API ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn create-documents-folder [data]
-  (pd/post (str "/documents/folders" data)))
+  (pd/post "/documents/folders" data))
 
 (defn rename-documents-folder [id data]
   (pd/put (str "/documents/folders/" id) data))
 
 (defn list-documents-folder []
-  (pd/get (str "/documents/folders/")))
+  (pd/get "/documents/folders/"))
 
 (defn create-templates-folder [data]
-  (pd/post (str "/templates/folders/" data)))
+  (pd/post "/templates/folders/" data))
 
 (defn rename-templates-folder [id data]
   (pd/put (str "/templates/folders/" id) data))
 
 (defn list-templates-folder []
-  (pd/get (str "/templates/folders/")))
+  (pd/get "/templates/folders/"))
